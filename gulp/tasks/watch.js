b03ba3c7ae18140a974002bfac084142a57a2d95
Maks Oneyear <mcsodn@gmail.com>
2018-07-09 15:48:59 +0500

    add threejs cube&sphere

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,6 +2,7 @@ module.exports = function () {
     $.gulp.task('watch',function () {
         $.gulp.watch('src/pug/**/*pug', $.gulp.series('pug')),
         $.gulp.watch('src/static/stylus/**/*.styl', $.gulp.series('stylus')),
-        $.gulp.watch('src/static/js/main.js', $.gulp.series('scripts'))
+        $.gulp.watch('src/static/js/main.js', $.gulp.series('scripts')),
+        $.gulp.watch('src/static/js/three/three_main.js', $.gulp.series('scripts'))
     });
 }
