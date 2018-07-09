e066108740141230f256162caad12723f554be59
Maks Oneyear <mcsodn@gmail.com>
2018-07-09 17:50:12 +0500

    add shadows

--- src/static/js/three/three_main.js
+++ src/static/js/three/three_main.js
@@ -3,38 +3,58 @@ $(function () {
     var camera = new THREE.PerspectiveCamera(45
         , window.innerWidth / window.innerHeight, 0.1, 1000);
     var renderer = new THREE.WebGLRenderer();
-    //renderer.setClearColorHex(0xEEEEEE);
+
     renderer.setClearColor(new THREE.Color(0xEEEEEE, 1.0));
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+    var spotLight = new THREE.SpotLight( 0xffffff );
+    spotLight.position.set( -40, 60, -10 );
+    spotLight.castShadow = true;
+
+    spotLight.shadow.mapSize.width = 8192;
+    spotLight.shadow.mapSize.height = 8192;
+    spotLight.shadow.camera.near = 0.5;
+    spotLight.shadow.camera.far = 500;
+
+    scene.add(spotLight);
 
     var axes = new THREE.AxisHelper(20);
     scene.add(axes);
 
     var planeGeometry = new THREE.PlaneGeometry(60, 20, 1, 1);
-    var planeMaterial = new THREE.MeshBasicMaterial({color: 0xcccccc});
+    var planeMaterial = new THREE.MeshLambertMaterial(
+        {color: 0xffffff});
     var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
     plane.rotation.x = -0.5 * Math.PI;
     plane.position.x = 15;
     plane.position.y = 0;
     plane.position.z = 0;
+    plane.receiveShadow = true;
     scene.add(plane);
 
     var cubeGeometry = new THREE.CubeGeometry(4, 4, 4);
-    var cubeMaterial = new THREE.MeshBasicMaterial(
-        {color: 0xff0000, wireframe: true});
+    var cubeMaterial = new THREE.MeshLambertMaterial(
+        {color: 0xff0000});
     var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
     cube.position.x = -4;
     cube.position.y = 3;
     cube.position.z = 0;
+    cube.castShadow = true;
+    cube.receiveShadow = false;
     scene.add(cube);
 
     var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-    var sphereMaterial = new THREE.MeshBasicMaterial(
-        {color: 0x7777ff, wireframe: true});
+    var sphereMaterial = new THREE.MeshLambertMaterial(
+        {color: 0x7777ff});
     var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
     sphere.position.x = 20;
     sphere.position.y = 4;
     sphere.position.z = 2;
+    sphere.castShadow = true;
+    sphere.receiveShadow = false;
     scene.add(sphere);
 
     camera.position.x = -30;
