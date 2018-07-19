185ad9ec72fde7c96ffca8f1f73a3017eb99cdf1
Maks Oneyear <mcsodn@gmail.com>
2018-07-19 17:29:25 +0500

    refactor three_main, add star bg

--- src/static/js/three/three_main.js
+++ src/static/js/three/three_main.js
@@ -1,116 +1,107 @@
-$(function () {
-    let scene = new THREE.Scene();
-    let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-    let renderer = new THREE.WebGLRenderer({ alpha: true });
-    let controls = new THREE.OrbitControls(camera, renderer.domElement);
+var scene, controls,
+    camera, fieldOfView, aspectRatio, nearPlane, farPlane,
+    renderer, container;
 
-    //renderer params
-    renderer.setClearColor(new THREE.Color("rgb(153, 153, 255)"), 0.5);
-    renderer.setSize(window.innerWidth, window.innerHeight);
+var HEIGHT, WIDTH;
+
+//создаем сцену
+
+function createScene() {
+    HEIGHT = window.innerHeight;
+    WIDTH = window.innerWidth;
+
+    scene = new THREE.Scene();
+    aspectRatio = WIDTH/HEIGHT;
+    fieldOfView = 60;
+    nearPlane = 1;
+    farPlane = 10000;
+
+    camera = new THREE.PerspectiveCamera(
+        fieldOfView,
+        aspectRatio,
+        nearPlane,
+        farPlane
+    );
+
+    camera.position.x = -30;
+    camera.position.y = 40;
+    camera.position.z = 30;
+    camera.lookAt(scene.position);
+    renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        antialias: true
+    });
+
+    renderer.setSize(WIDTH,HEIGHT);
     renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-    // Trackball controller
+    //add controls
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
     controls.dampingFactor = 0.25;
 
+    //Add axes
+    var axes = new THREE.AxesHelper(20);
+    scene.add(axes);
 
-    //Lighting
-    let spotLight = new THREE.SpotLight(0xffffff);
+    container = document.getElementById('WebGL-output');
+    container.appendChild(renderer.domElement);
+
+    window.addEventListener('resize', handleWindowResize,false);
+}
+
+//функция перерисовывает сцену, если изменились размеры окна
+
+function handleWindowResize() {
+    HEIGHT = window.innerHeight;
+    WIDTH = window.innerWidth;
+    renderer.setSize(HEIGHT,WIDTH);
+    camera.aspect = WIDTH/HEIGHT;
+    camera.updateProjectionMatrix();
+}
+
+//добавляем свет
+
+var spotLight;
+
+function createLights() {
+    spotLight = new THREE.SpotLight(0xffffff);
     spotLight.position.set( -40, 60, -10 );
     spotLight.castShadow = true;
 
     spotLight.shadow.mapSize.width = 512*4;
     spotLight.shadow.mapSize.height = 512*4;
-    spotLight.shadow.camera.near = 0.5;
-    spotLight.shadow.camera.far = 500;
+    spotLight.shadow.camera.near = 1;
+    spotLight.shadow.camera.far = 1000;
     spotLight.shadow.bias = -0.01;
 
     scene.add(spotLight);
-
-    //Add axes
-    let axes = new THREE.AxesHelper(20);
-    scene.add(axes);
-
-    //add ball
-    function loadCollada (collada) {
-        let ball = collada.scene;
-
-        //add ball's shadow
-        //не побеждена проблема тени самой на себя
-        // ball.traverse(function(child) {
-        //     child.castShadow = true;
-        //     child.receiveShadow = true;
-        // });
-
-        scene.add(ball);
-    }
-
-    let loaderColladaBall = new THREE.ColladaLoader();
-    loaderColladaBall.load('static/models/ballmodel.dae',loadCollada);
+}
 
 
-    //add ground
-    let planeGeometry = new THREE.PlaneGeometry(20, 20, 1, 1);
-    let planeMaterial = new THREE.MeshLambertMaterial(
-        {color: 0xffffff});
-    let plane = new THREE.Mesh(planeGeometry, planeMaterial);
+Planet = function () {
+    var geometry = new THREE.SphereGeometry(8, 20, 20);
+    var material = new THREE.MeshLambertMaterial(
+         {color: 0x7777ff});
+    this.mesh = new THREE.Mesh(geometry, material);
+    this.mesh.receiveShadow = true;
+}
 
-     plane.rotation.x = -0.5 * Math.PI;
-    // plane.position.x = 15;
-    plane.position.x = 0;
-    plane.position.y = 0;
-    plane.position.z = 0;
-    plane.receiveShadow = true;
-    scene.add(plane);
+function createPlanet() {
+    var planet = new Planet();
+    scene.add(planet.mesh);
+}
 
-    //add cube
-    // let cubeGeometry = new THREE.CubeGeometry(4, 4, 4);
-    // let cubeMaterial = new THREE.MeshLambertMaterial(
-    //     {color: 0xff0000});
-    // let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    // cube.position.x = -4;
-    // cube.position.y = 3;
-    // cube.position.z = 0;
-    // cube.castShadow = true;
-    // cube.receiveShadow = false;
-    // scene.add(cube);
-    //
-    // //add sphere
-    // let sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-    // let sphereMaterial = new THREE.MeshLambertMaterial(
-    //     {color: 0x7777ff});
-    // let sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-    // sphere.position.x = 20;
-    // sphere.position.y = 4;
-    // sphere.position.z = 2;
-    // sphere.castShadow = true;
-    // sphere.receiveShadow = false;
-    // scene.add(sphere);
+function loop() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(loop);
+}
 
-    //add camera
-    camera.position.x = -30;
-    camera.position.y = 40;
-    camera.position.z = 30;
-    camera.lookAt(scene.position);
+function init() {
+    createScene();
+    createLights();
+    createPlanet();
+    loop();
+}
 
-    //output to DOM
-    $("#WebGL-output").append(renderer.domElement);
-    renderScene();
-
-
-    function renderScene() {
-        requestAnimationFrame(renderScene);
-        renderer.render(scene, camera);
-        controls.update();
-    }
-
-    window.addEventListener( 'resize', onWindowResize, false );
-
-    function onWindowResize() {
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        renderScene();
-    }
-});
+window.addEventListener('load', init, false);
