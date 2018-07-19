f8de25d0c4919af713f4e7e6bdca7ccf7d6d22bd
Maks Oneyear <mcsodn@gmail.com>
2018-07-19 12:04:03 +0500

    upd gulp-img.js

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -1,7 +1,7 @@
 module.exports = function () {
     $.gulp.task('img', function () {
         return $.gulp.src('src/static/img/**/*.{png,jpg,jpeg,gif}')
-            .pipe($.gp.tinypng('v1ehPsZ-RHfMWp-99T2fDqP9gjP-1rNB'))
+            .pipe($.gulpPlugins.tinypng('v1ehPsZ-RHfMWp-99T2fDqP9gjP-1rNB'))
             .pipe($.gulp.dest('build/static/img/'));
     });
 }
